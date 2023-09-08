243f16ae8312e9af4af91ef3bde4521ddb8fe37f
unknown <dinilgamage5@gmail.com>
2023-09-08 17:30:36 +0530

    Initial Commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth'); //makes sure a user is logged in before returing the view for home screen
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')  <!-- go into the layouts folder and grab the templete there, import it into this blade file-->
+
+@section('content') <!-- reorganize into an external file, go to layouts.app to find where it yields (basically this section is replaced instead of yeild('content'))-->
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
