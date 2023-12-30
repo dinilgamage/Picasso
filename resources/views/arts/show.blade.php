3609bd7e5f633d6b5cf01bdc7ed5045b1a5ee956
Dinil <dinilgamage5@gmail.com>
2023-12-30 13:29:38 +0530

    added artist details to artwork view

--- resources/views/arts/show.blade.php
+++ resources/views/arts/show.blade.php
@@ -1,15 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>{{ $artwork->title }}</h1>
-    <img src="{{ asset('images/' . $artwork->image) }}" alt="{{ $artwork->title }}">
+    <div class="container">
+        <h1>{{ $artwork->title }}</h1>
 
-    <!-- Display the art's details here -->
+        <div class="row">
+            <div class="col-md-6">
+                <div style="width: 500px; height: 500px; overflow: hidden;">
+                    <img src="{{ asset('images/' . $artwork->image) }}" alt="{{ $artwork->title }}" style="object-fit: contain; width: 100%; height: 100%;">
+                </div>
+            </div>
 
-    <a href="#" class="btn btn-primary">Add to cart</a>
-    <a href="#" class="btn btn-primary">Purchase</a>
-    <a href="{{ route('arts.main') }}" class="btn btn-danger">Back</a>
+            <div class="col-md-6">
+                {{-- details div --}}
+                <div> 
+                    <p>Description: {{ $artwork->desc }}</p>
+                    <p>Artist: {{ $artwork->user->name }}</p>
+                    <p>Category: {{ $artwork->category->name }}</p>
+                    <p>Price: {{ $artwork->price }}</p>
+                    <p>Material: {{ $artwork->material }}</p>
+                    <p>Size: {{ $artwork->size }}</p>
+                    <p>Frame: {{ $artwork->frame }}</p>
+                    <p>Signature: {{ $artwork->signature }}</p>
+                </div>
 
-</div>
+                {{-- buttons div --}}
+                <div>
+                    <a href="#" class="btn btn-primary">Add to cart</a>
+                    <a href="#" class="btn btn-primary">Add to wishlist</a>
+                    <a href="{{ route('artists.show', $artwork->user->id) }}" class="btn btn-primary">View artist</a>
+                    <a href="#" class="btn btn-primary">Purchase</a>
+                    <a href="{{ route('arts.main') }}" class="btn btn-danger">Back</a>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
