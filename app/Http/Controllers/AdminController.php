bdf480d0328e40e6235e740e8b858c00fb54f109
Dinil <dinilgamage5@gmail.com>
2024-01-25 01:03:03 +0530

    admin analytics implemented

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,61 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Artwork;
+use App\Models\Category;
+use App\Models\OrderItem;
+use App\Models\Rating;
+
 
 class AdminController extends Controller
 {
     public function index()
-{
-    return view('admin.dashboard');
-}
+    {
+            $totalUsersWithAdmin = User::count();
+            $totalUsers = $totalUsersWithAdmin - 1;
+            $totalCategories = Category::count();
+            $totalArtworks = Artwork::where('sold', '!=', 1)->count();
+            $totalOrders = OrderItem::count();
+            $totalRatings = Rating::count();
+
+            $totalArtworksListed = Artwork::count();
+            $totalArtworksSold = Artwork::where('sold', 1)->count();      
+            $totalArtworksOnSale = $totalArtworksListed - $totalArtworksSold;
+
+            $totalProfileViews = User::sum('profile_views');
+            $totalArtworkViews = Artwork::sum('views');
+
+
+            $categoryNames = [];
+            $categoryViewsData = [];
+            $categoriesViews = Artwork::with('category')
+                ->select('category_id', \DB::raw('sum(views) as total_views'))
+                ->groupBy('category_id')
+                ->get();
+            foreach ($categoriesViews as $categoryView) {
+                $categoryName = $categoryView->category ? $categoryView->category->name : 'Unknown';
+                $categoryNames[] = $categoryName;
+                $categoryViewsData[] = $categoryView->total_views;
+            }
+
+            return view('admin.dashboard', compact(
+                'totalUsers', 
+                'totalArtworks', 
+                'totalCategories', 
+                'totalOrders', 
+                'totalRatings',
+                'totalProfileViews',
+                'totalArtworkViews',
+                'totalArtworksSold',
+                'totalArtworksOnSale',
+                'totalArtworksListed',
+                'categoryNames',
+                'categoryViewsData',
+                
+                
+            ));
+        
+    }
 
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,145 @@
-@extends('layouts.admindash') 
+@extends('layouts.admindash')
 
 @section('content')
+
 <div class="container">
-    <h1>Welcome, {{ Auth::user()->name }}</h1>
     
-   
+    <div class="row justify-content-center">
+        <h1>Dashboard</h1>
+        <div class="row">
+            <div class="col-md-2">
+                <div class="card text-center mb-3 card-outer">
+                    <div class="card-body card-inner">
+                        <h5 class="card-title">{{ $totalUsers }}</h5>
+                        <p class="card-text">Total Users</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                {{-- <div class="card text-center mb-3 card-outer">
+                    <div class="card-body card-inner">
+                        <h5 class="card-title">{{ $totalArtworks }}</h5>
+                        <p class="card-text">Total Artworks On Sale</p>
+                    </div>
+                </div> --}}
+                <div class="card text-center mb-3 card-outer">
+                    <div class="card-body card-inner">
+                        <h5 class="card-title">{{ $totalArtworkViews }}</h5>
+                        <p class="card-text">Artwork Clicks</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card text-center mb-3 card-outer">
+                    <div class="card-body card-inner">
+                        <h5 class="card-title">{{ $totalProfileViews }}</h5>
+                        <p class="card-text">Profile Clicks</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card text-center mb-3 card-outer">
+                    <div class="card-body card-inner">
+                        <h5 class="card-title">{{ $totalCategories }}</h5>
+                        <p class="card-text">Total Categories</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card text-center mb-3 card-outer">
+                    <div class="card-body card-inner">
+                        <h5 class="card-title">{{ $totalRatings }}</h5>
+                        <p class="card-text">Ratings Submitted</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="card text-center mb-3 card-outer">
+                    <div class="card-body card-inner">
+                        <h5 class="card-title">{{ $totalOrders }}</h5>
+                        <p class="card-text">Total Orders Placed</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <canvas width="400" id="artworksPieChart"></canvas>
+
+            </div>
+            <div class="col-md-8">
+                <canvas id="categoryViewsBarChart"></canvas>
+
+            </div>
+        </div>
+
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        var ctx = document.getElementById('artworksPieChart').getContext('2d');
+        var artworksPieChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['Artworks On Sale', 'Artworks Sold'],
+                datasets: [{
+                    label: 'Artworks Distribution',
+                    data: [@json($totalArtworksOnSale), @json($totalArtworksSold)],
+                    backgroundColor: [
+                        'rgba(147, 56, 0, 1)',
+                        'rgba(255, 187, 7, 1)'
+                    ],
+                    borderColor: [
+                        'rgba(147, 56, 0, 1)',
+                        'rgba(255, 187, 7, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: false,
+                plugins: {
+                    legend: {
+                        position: 'top',
+                    },
+                    title: {
+                        display: true,
+                        text: 'Artworks Distribution'
+                    }
+                }
+            },
+        });
+
+        var ctx = document.getElementById('categoryViewsBarChart').getContext('2d');
+        var categoryViewsBarChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: @json($categoryNames),
+                datasets: [{
+                    label: 'Total Views by Category',
+                    data: @json($categoryViewsData),
+                    backgroundColor: 'rgba(147, 56, 0, 1)',
+                    borderColor: 'rgba(147, 56, 0, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                },
+                responsive: true,
+                plugins: {
+                    legend: {
+                        display: false
+                    },
+                    title: {
+                        display: true,
+                        text: 'Total Views by Category'
+                    }
+                }
+            },
+        });
+    </script>
 </div>
 @endsection
